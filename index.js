20c9e0a667167c83ded3c7d1facac044144c70bc
Aximilli1212 <aximill1212@gmail.com>
2019-11-25 18:25:54 +0000

    return single playlist

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const express = require("express")
-const Sequelize = require("sequelize")
+// const Sequelize = require("sequelize")
+const Playlist = require('./models/playlist');
 
 const app = express();
-const sequelize = new Sequelize('sqlite:chinook.db');
-
-//Create model
-const Playlist = sequelize.define('playlist',{
-    id:{
-        field:'PlaylistId',
-        type:Sequelize.INTEGER,
-        primaryKey:true
-    },
-    name: {
-        field: 'Name',
-        type: Sequelize.STRING,
-    },
-},
-    {
-       timestamps: false
-    })
 
 //Get all Playlists
 app.get('/api/playlists', function(req,res){
     Playlist.findAll().then(response=>{
+        res.json(response);
+    })
+})
+//Get Single Playlist
+app.get('/api/playlists/:id', function(req,res){
+    let {id} = req.params;
+    Playlist.findByPk(id).then(response=>{
         res.json(response);
     })
 })
